f5652f9b37ba945887b0132109d9811d74aefa2e
SiyaM108 <deprominent@gmail.com>
2025-12-09 16:31:05 +0000

    complete BoxColor.js

---
+++ src/BoxColor.js
@@ -0,0 +1,17 @@
+function BoxColor({ r, g, b }) {
+  const rgb = `rgb(${r},${g},${b})`;
+  const hex =
+    "#" +
+    [r, g, b]
+      .map((x) => x.toString(16).padStart(2, "0"))
+      .join("");
+
+  return (
+    <div style={{ backgroundColor: rgb, padding: "20px", border: "1px solid black" }}>
+      <p>{rgb}</p>
+      <p>{hex}</p>
+    </div>
+  );
+}
+
+export default BoxColor;
